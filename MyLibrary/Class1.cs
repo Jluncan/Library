54b9f44d7d129efdd293ba13b4f54c732b9279e1
James Luncan <jluncan@tql.com>
2024-06-19 10:19:35 -0400

    Add project files.

---
+++ MyLibrary/Class1.cs
@@ -0,0 +1,8 @@
+﻿namespace MyLibrary {
+    public class Class1 {
+        public static string About { get; set; } = "MyClass1";
+
+        public string About2 { get; set; } = "Myclass1 & About 2"
+
+    }
+}
